e1bcfd70d8fc656346ff006ac2cd0e52074c9dda
miqsargsyan21 <michael.sargs1an@gmail.com>
2021-12-16 13:26:57 +0400

    Changed something

--- Layouts/MainLayout/index.js
+++ Layouts/MainLayout/index.js
@@ -14,15 +14,17 @@ export default function MainLayout ({children, pageTitle}) {
             <Head>
                 <title>{pageTitle ? pageTitle : 'My app'}</title>
             </Head>
-            <nav className={'flex flex-row justify-around items-center bg-blue-400 text-white font-3xl h-min dark:bg-black'}>
-                <Link href={"/"}><a className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Home</a></Link>
-                <Link href={"/posts"}><a  className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Posts</a></Link>
-                <Link href={"/contact"}><a  className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Contact</a></Link>
-                <button className={`w-min py-3 px-2 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600`} onClick={handleChangingMode}>
-                    <p>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</p>
-                </button>
-            </nav>
-            {children}
+            <div className={'h-screen dark:bg-gray-500'}>
+                <nav className={'flex flex-row justify-around items-center bg-blue-400 text-white font-3xl h-min dark:bg-black'}>
+                    <Link href={"/"}><a className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Home</a></Link>
+                    <Link href={"/posts"}><a  className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Posts</a></Link>
+                    <Link href={"/contact"}><a  className={'py-6 px-20 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600'}>Contact</a></Link>
+                    <button className={`w-min py-3 px-2 rounded-md hover:bg-blue-600 dark:hover:bg-gray-600`} onClick={handleChangingMode}>
+                        <p>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</p>
+                    </button>
+                </nav>
+                {children}
+            </div>
         </div>
     )
 }
